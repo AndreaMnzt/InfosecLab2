5db199cf383d8d8aa6e677c4db72866d4d0606c1
dlazzaro <dmlazzaro98@gmail.com>
2020-11-28 18:22:20 +0100

    task 5 update on BSC function

--- task5.py
+++ task5.py
@@ -8,27 +8,19 @@ class Results:
         self.errors = errors
 
 def BSC(message, error):
-    #mean = len(message) * error
-    #variance = mean * (1 - error)
-    #standard_dev = m.sqrt(variance)
-    #number_errors = np.random.normal(mean, standard_dev)
-    number_errors_binomial=np.random.binomial(len(message), error)
-    #print(number_errors_binomial)
-    #print(number_errors)
-    user_message = np.array(message)
-    flag_array = np.zeros(len(message))
 
-    #print(x)
-    for i in range(number_errors_binomial):
-        position = r.randint(0, len(user_message) - 1)
-        if flag_array[position] == 0:
-            user_message[position] = 1 if user_message[position] == 0 else 0
-            flag_array[position] = -1
-        else:
-            i = i - 1
+    number_errors_binomial=np.random.binomial(len(message), error)
+
+    user_message = np.array(message)
+
+    errors_array=np.random.choice(len(message)-1, number_errors_binomial, replace=False)
+    for i in range(len(errors_array)):
+        index=errors_array[i]
+        user_message[index] = 1 if user_message[index] == 0 else 0
+
     return Results(user_message, number_errors_binomial)
 
-    #np.random.choice(n, sie=zerror_bits, replace=False)
+
 
 
 
